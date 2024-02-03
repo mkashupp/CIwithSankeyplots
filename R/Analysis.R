e9385ca93c0bf38a8159ace9ef69dcaaf5b1b599
kashupp <kashifbioinfo@gmail.com>
2024-02-03 01:56:48 +0100

    Initial commit

---
+++ R/Analysis.R
@@ -0,0 +1,12 @@
+rm(list=ls())
+
+creatData <- function(datalenght=5 )
+{
+  raw.data <- matrix( rep(1:datalenght,datalenght), nrow=datalenght, ncol=datalenght)
+  return <- raw.data
+}
+
+sumamrizeData <- fucntion( rdata )
+{
+  print(rdata)
+}
